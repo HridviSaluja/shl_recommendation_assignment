c9403729673a21c8d43861c9e8dadfdd468a2fe7
Hridvi Saluja <85825530+HridviSaluja@users.noreply.github.com>
2025-11-10 19:40:25 +0530

    Fix API_URL declaration in client.py

--- client.py
+++ client.py
@@ -2,7 +2,8 @@ import streamlit as st
 import requests
 import json
 
-API_URL = "API_URL = "https://shl-recommendation-assignment-new.onrender.com/recommend"
+API_URL = "https://shl-recommendation-assignment-new.onrender.com/recommend"
+
 
 st.set_page_config(page_title="SHL Assessment Recommender", layout="centered")
 
